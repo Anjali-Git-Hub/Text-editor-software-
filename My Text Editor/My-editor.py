a8b5a97fa716575bf892d7451ae28faff25271a2
Anjali <122084921+Anjali-Git-Hub@users.noreply.github.com>
2023-09-15 11:54:38 +0530

    added view and edit dropdown menu icons

--- My Text Editor/My-editor.py
+++ My Text Editor/My-editor.py
@@ -26,10 +26,28 @@ file.add_command(label='Save',image=save_icon,compound=tk.LEFT,accelerator='Ctrl
 file.add_command(label='Save As',image=saveas_icon,compound=tk.LEFT,accelerator='Ctrl+Alt+S')
 file.add_command(label='Exit',image=exit_icon,compound=tk.LEFT,accelerator='Ctrl+Q')
 
-
+# Edit dropdown menu icons
+copy_icon=tk.PhotoImage(file='icon/copy.png')
+paste_icon=tk.PhotoImage(file='icon/paste.png')
+cut_icon=tk.PhotoImage(file='icon/cut.png')
+clearall_icon=tk.PhotoImage(file='icon/clear_all.png')
+find_icon=tk.PhotoImage(file='icon/find.png')
 
 edit=tk.Menu(main_menu,tearoff=False)
+edit.add_command(label='Copy',image=copy_icon,compound=tk.LEFT,accelerator='Ctrl+C')
+edit.add_command(label='Paste',image=paste_icon,compound=tk.LEFT,accelerator='Ctrl+V')
+edit.add_command(label='Cut',image=cut_icon,compound=tk.LEFT,accelerator='Ctrl+X')
+edit.add_command(label='Clear All',image=clearall_icon,compound=tk.LEFT,accelerator='Ctrl+Alt+C')
+edit.add_command(label='Find',image=find_icon,compound=tk.LEFT,accelerator='Ctrl+F')
+
+# View dropdown menu icons 
+toolbar_icon=tk.PhotoImage(file='icon/tool_bar.png')
+statusbar_icon=tk.PhotoImage(file='icon/status_bar.png')
+
 view=tk.Menu(main_menu,tearoff=False)
+view.add_checkbutton(label="Tool Bar",image=toolbar_icon,compound=tk.LEFT,accelerator='')
+view.add_checkbutton(label="Status Bar",image=statusbar_icon,compound=tk.LEFT,accelerator='')
+
 color=tk.Menu(main_menu,tearoff=False)
 
 main_menu.add_cascade(label='File', menu=file)
